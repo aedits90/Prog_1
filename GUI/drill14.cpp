a797299d5782a2c8c61e0916edd5d63e784aaae1
aedits90 <55504148+aedits90@users.noreply.github.com>
2021-05-06 08:24:44 +0200

    Update drill14.cpp

--- GUI/drill14.cpp
+++ GUI/drill14.cpp
@@ -1,64 +1,69 @@
-#include <iostream>
-#include <string>
+#include "std_lib_facilities.h"
 
-using std::cout;
-using std::string;
-
-struct B1 {
+class B1 {
+public:
     virtual void vf() { cout << "B1::vf()\n"; }
     void f() { cout << "B1::f()\n"; }
     virtual void pvf() =0;
 };
 
-struct D1 : B1 {
-    void vf() override { cout << "D1::vf()\n"; }
-    void f() { cout << "D1::f()\n"; }
+class D1 : public B1 {
+public:
+    void vf() { cout << "D1::vf()\n"; }
+    //void f() { cout << "D1::f()\n"; }
 };
 
-struct D2 : D1 {
-    void pvf() override { cout << "D2::pvf()\n"; }
+class D2 : public D1 {
+public:
+    void pvf() { cout << "D2::pvf()\n"; }
 };
 
-struct B2 {
+class B2 {
+public:
     virtual void pvf() =0;
 };
 
-struct D21 : B2 {
-    string s = "stuffs";
-    void pvf() override { cout << s << '\n'; }
+class D21 : public B2 {
+public:
+    void pvf() { cout << s << endl; }
+    string s;
 };
 
-struct D22 : B2 {
-    int i = 37;
-    void pvf() override { cout << i << '\n'; }
+class D22 : public B2 {
+public:
+    void pvf() { cout << n << endl;}
+    int n;
 };
 
-void f (B2& b)
+void f(B2& b2)
 {
-    b.pvf();
+    b2.pvf();
 }
 
-int main()
-{
-    /*
-    B1 b;
-    b.vf();
-    b.f();
-    D1 d;
-    d.vf();
-    d.f();
-    B1& r {d};
-    r.vf();
-    r.f();
-    D2 dd;
-    dd.vf();
-    dd.f();
-    dd.pvf();
-    */
+int main() {
+    //B1 b1;
+    //b1.vf();
+    //b1.f();
 
-    D21 two_one;
-    D22 two_two;
+    //D1 d1;
+    //d1.vf();
+    //d1.f();
 
-    f(two_one);
-    f(two_two);
+    //B1& bref = d1;
+    //bref.vf();
+    //bref.f();
+
+    D2 d2;
+    d2.f();
+    d2.vf();
+    d2.pvf();
+
+    D21 d21;
+    d21.s = "d21.s";
+    D22 d22;
+    d22.n = 22;
+    f(d21);
+    f(d22);
+
+    keep_window_open();
 }
